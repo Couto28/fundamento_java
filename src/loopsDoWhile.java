74b88411c31b18e6581d326e3c4c3b3a80f6bab1
Couto28 <di.diogocouto@gmail.cmo>
2022-04-08 08:56:54 -0300

    novo

---
+++ src/loopsDoWhile.java
@@ -0,0 +1,16 @@
+public class loopsDoWhile {
+
+    public static void main(String[] Args){
+
+        // do while , ele excuta o trexo de código ao menos uma vez , mesmo que seja falso ele será excutado .
+
+        int y = 15;
+
+        do {
+            System.out.println(" Executando código . . . !");
+            y++;
+        } while (y > 16); // Se o código for verdadeiro ele será excutado variás vezes .
+                          // se for falso ele excuta apenas uma vez .
+
+    }
+}
