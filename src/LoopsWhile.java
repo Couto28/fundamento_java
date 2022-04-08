929a3c0ab4cfefd0ebd9d8e3fed27449ac12fbf3
Couto28 <di.diogocouto@gmail.cmo>
2022-04-08 08:41:42 -0300

    novo commit

---
+++ src/LoopsWhile.java
@@ -0,0 +1,12 @@
+public class LoopsWhile {
+
+    public static void main(String[] Args){
+
+        int y = 10;
+
+        while ( y < 15){
+            System.out.println(" Executou : " + y);
+            y ++;  // encremento
+        }
+    }
+}
